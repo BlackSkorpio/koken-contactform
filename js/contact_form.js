ffb7be7971f6971bd7e4dc34ba355ec8ef3b9dc0
Bjarne Varöystrand <black.skorpio+github@gmail.com>
2015-03-21 17:33:43 +0100

    Första Github versionen

---
+++ js/contact_form.js
@@ -0,0 +1,24 @@
+﻿$(document).ready(function(){
+$("#ajax-contact-form").submit(function(){
+var str = $(this).serialize();
+$.ajax({
+  type: "POST",
+  url: "/contactform/contact.php",
+  data: str,
+  success: function(msg){
+    $("#note").ajaxComplete(function(event, request, settings){
+    if(msg == 'OK') {
+      result = '<div class="notification_ok">Ditt meddelande har skickats.<br />You mail was sent. Thank you!</div>';
+      $("#fields").hide();
+    }
+    else {
+      result = msg;
+    }
+    $(this).hide();
+    $(this).html(result).slideDown("slow");
+    $(this).html(result);
+  }); }
+});
+return false;
+});
+});
