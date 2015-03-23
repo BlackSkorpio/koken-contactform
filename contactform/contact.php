2dc605ad98989ee90ee04b38cd44520c9df9423f
Bjarne Varöystrand <black.skorpio+github@gmail.com>
2015-03-23 20:15:52 +0100

    Moved some stuff

--- contactform/contact.php
+++ contactform/contact.php
@@ -1,10 +1,8 @@
 <?php
-// contact.php version: 2015032320:05
+// contact.php version: 2015032320:14
 // MUST BE ANSI NOT UTF-8
 // Uncomment for trouble shooting
 //error_reporting (E_ALL ^ E_NOTICE);
-
-
 // Make sure that we get the settings
 include ('settings.php');
 // Generate correct date and mail id
@@ -14,14 +12,12 @@ $mid = "<" . sha1(microtime()) . "@" . $_SERVER['HTTP_HOST'] . ">";
 $post = (!empty($_POST)) ? true : false;
 if ($post) {
   $name = stripslashes($_POST['name']);
-  // Ensure that we keep foregin charaters intact in the name
-  $isoname= iconv("UTF-8", "ISO-8859-1", $name);
   $email = trim($_POST['email']);
   $message = stripslashes($_POST['message']);
-  // Ensure that we keep foregin chararters intact mail body
-  $isomessage = iconv("UTF-8", "ISO-8859-1", $message);
   $subject = stripslashes($_POST['subject']);
-  // Ensure that we keep foregin chararters intact in the mail subject
+  // Ensure that we keep foregin charaters intact in the name subject &message 
+  $isoname = iconv("UTF-8", "ISO-8859-1", $name);
+  $isomessage = iconv("UTF-8", "ISO-8859-1", $message);
   $isosubject = iconv("UTF-8", "ISO-8859-1", $sub_open.$subject);
 
 $headers = <<<EOT
