e533bbd484c4ac78c0e55e9f79f2fda7c9150c68
Bjarne Varöystrand <black.skorpio+github@gmail.com>
2015-03-23 20:08:58 +0100

    Cleaned up mail headers in contact.php

--- contactform/contact.php
+++ contactform/contact.php
@@ -1,12 +1,11 @@
 <?php
-// contact.php version: 2015032213:31
+// contact.php version: 2015032320:05
 // MUST BE ANSI NOT UTF-8
 // Uncomment for trouble shooting
 //error_reporting (E_ALL ^ E_NOTICE);
 
 
 // Make sure that we get the settings
-//require_once($_SERVER['DOCUMENT_ROOT'].'/contactform/settings.php');
 include ('settings.php');
 // Generate correct date and mail id
 $date = date(DATE_RFC2822);
@@ -24,8 +23,8 @@ if ($post) {
   $subject = stripslashes($_POST['subject']);
   // Ensure that we keep foregin chararters intact in the mail subject
   $isosubject = iconv("UTF-8", "ISO-8859-1", $sub_open.$subject);
-/*
-$headers - <<<EOT
+
+$headers = <<<EOT
 From: $isoname <$email>
 Reply-To: $email
 Cc: $email
@@ -36,19 +35,7 @@ Content-Type: text/plain; charset="UTF-8"
 Content-Transfer-Encoding: quoted-printable
 EOT;
 		$mail = $mailfunction("$recipientname <$recipient>", $isosubject, $isomessage, $headers, "-f$email");
-*/
 
-		$mail = $mailfunction("$recipientname <$recipient>", $isosubject, $isomessage,
-               "From: ".$isoname." <".$email.">\r\n"
-              ."Reply-To: ".$email."\r\n"
-			  ."Cc: ".$email."\r\n"
-			  ."Date: ".$date."\r\n"
-			  ."Message-ID: ".$mid."\r\n"
-			  ."MIME-Version: 1.0\r\n"
-              ."Content-Type: text/plain; charset=UTF-8\r\n"
-			  ."Content-Transfer-Encoding: quoted-printable\r\n"
-			  ,"-f$email"
-			  ); 
-  if ($mail) { echo 'OK'; } else { echo '<div class="notification_error">'.$error_notice.'</div>'; }
+	if ($mail) { echo 'OK'; } else { echo '<div class="notification_error">'.$error_notice.'</div>'; }
 }
 ?>
